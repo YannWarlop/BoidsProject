3bf883813ff32726c24d133974b32d12b0b4aacc
Yann-Warlop <yann.warlop@outlook.fr>
2025-03-26 18:31:53 +0100

    So fucking back

--- Assets/Scripts/3D/BoidBehaviour3D.cs
+++ Assets/Scripts/3D/BoidBehaviour3D.cs
@@ -5,7 +5,40 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Serialization;
 
-public class BoidBehaviour3D : MonoBehaviour { 
+public class BoidBehaviour3D : MonoBehaviour
+{
+    [Header("Boid Settings")]
+    [SerializeField] private float _boidSpeed;
+    [SerializeField] private float _meanDirForce;
     
+    // Base Data for Compute Shader
+    public Vector3 position; // Boid Position
+    public Vector3 direction; // Boid Direction
+
+    //Updated By Compute Shader
+    public Vector3 meanSOIDirection; // Average Boid Direction in Sphere Of Influence 
+
+    private void Update() {
+        BoidUpdate();
+    }
+
+    public void BoidUpdate() {
+        Vector3 dirSum = Vector3.zero; //Reset the Direction
+        
+        //Apply the different Influences
+        dirSum += direction;
+        dirSum += meanSOIDirection.normalized *_meanDirForce;
+        
+        
+        dirSum.Normalize(); //After all influences are set, normalize the Direction
+        //Move the Boid
+        transform.position += dirSum * _boidSpeed * Time.deltaTime; //Move
+        transform.LookAt(transform.position + dirSum); //Adjust Rotation
+        
+        //Actualize Data for next Compute
+        position = transform.position;
+        direction = dirSum;
+    }
 }
+
 
--- Assets/Scripts/3D/BoidsManager3D.cs
+++ Assets/Scripts/3D/BoidsManager3D.cs
@@ -2,6 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoidsManager3D : MonoBehaviour {
+public class BoidsManager3D : MonoBehaviour
+{
+    [Header("Spawn")] 
+    [SerializeField] private GameObject boidPrefab;
+    [SerializeField] private int boidCount;
+    [SerializeField] private float boidSpawnRadius;
 
+    private void Awake() {
+        for (int i = 0; i < boidCount; i++) {
+            Vector3 _spawnPoint = new Vector3( //Random Point in Spawn Radius
+                Random.Range(-boidSpawnRadius, boidSpawnRadius), 
+                Random.Range(-boidSpawnRadius, boidSpawnRadius), 
+                Random.Range(-boidSpawnRadius, boidSpawnRadius)); 
+            Vector3 _spawnDirection = new Vector3( //Random Direction
+                Random.Range(0,360),
+                Random.Range(0,360),
+                Random.Range(0,360)); 
+             
+            var _boid = Instantiate(boidPrefab, _spawnPoint, Quaternion.Euler(_spawnDirection)); //Store GameObject 
+            var _boidComponent = _boid.GetComponent<BoidBehaviour3D>(); //Store Component
+            _boidComponent.position = _spawnPoint; //Initialize public variables for Compute
+            _boidComponent.direction = _spawnDirection;
+        }
+    }
 }
