79d383e20f8279136f8f52c100be669223ee09d5
Yann-Warlop <yann.warlop@outlook.fr>
2025-03-27 11:57:09 +0100

    ALLE ON Y RETOURNE

--- Assets/Scripts/3D/BoidBehaviour3D.cs
+++ Assets/Scripts/3D/BoidBehaviour3D.cs
@@ -10,6 +10,8 @@ public class BoidBehaviour3D : MonoBehaviour
     [Header("Boid Settings")]
     [SerializeField] private float _boidSpeed;
     [SerializeField] private float _meanDirForce;
+    [SerializeField] private float _colAvoidForce;
+    [SerializeField] private float _fovRadius;
     
     // Base Data for Compute Shader
     public Vector3 position; // Boid Position
@@ -17,7 +19,28 @@ public class BoidBehaviour3D : MonoBehaviour
 
     //Updated By Compute Shader
     public Vector3 meanSOIDirection; // Average Boid Direction in Sphere Of Influence 
+    
+    //For Sphere Collision Check
+    [Header("Spherical Collision Settings")]
+    [SerializeField] private int _collisionNumbers;
+    private Vector3[] _collisionDirections;
 
+    private void Start()
+    {
+        //Precalculate Spherical collision points
+        //Could be more optimized if calculated on a single instance instead of every boid
+        //Singleton could be possible
+        _collisionDirections = new Vector3[_collisionNumbers]; //Array of Points to check from the boid to them
+        float angleIncrement = Mathf.PI * 2 * ((1 + Mathf.Sqrt(5)) / 2); // Calculate incrementation of points
+        for (int i = 0; i < _collisionNumbers; i++) {
+            float phi = Mathf.Acos(1 - 2 * ((float)i / _collisionNumbers)); //Angles
+            float theta = angleIncrement * i;
+            //Angles to Coordinates via pherical projection
+            float x = Mathf.Sin(phi) * Mathf.Cos(theta), y = Mathf.Sin(phi) * Mathf.Sin(theta), z = Mathf.Cos(phi);
+            //Store position in array
+            _collisionDirections[i] = new Vector3(x, y, z);
+        }
+    }
     private void Update() {
         BoidUpdate();
     }
@@ -29,15 +52,48 @@ public class BoidBehaviour3D : MonoBehaviour
         dirSum += direction;
         dirSum += meanSOIDirection.normalized *_meanDirForce;
         
+        //AvoidCollision
+        if (Physics.Raycast(transform.position, direction, _fovRadius)) { //If Obstacle
+            dirSum += FindBestDirection() * _colAvoidForce;
+        } else {
+            Debug.DrawRay(transform.position, direction * _fovRadius, Color.green);
+        }
         
-        dirSum.Normalize(); //After all influences are set, normalize the Direction
-        //Move the Boid
-        transform.position += dirSum * _boidSpeed * Time.deltaTime; //Move
+        //Adjust Values
+        dirSum = dirSum.normalized;
+        transform.position += dirSum.normalized * _boidSpeed * Time.deltaTime; //Move the Boid
         transform.LookAt(transform.position + dirSum); //Adjust Rotation
         
         //Actualize Data for next Compute
         position = transform.position;
         direction = dirSum;
+    }
+
+    private bool CollisionCheck() { //Simple Raycast Forward
+        if (Physics.Raycast(transform.position, transform.forward, _fovRadius)) return true; // If Heading for collision Return Bool
+        else return false;
+    }
+
+    private Vector3 FindBestDirection() {
+        //Plot points on Sphere
+        Vector3 bestDirection = transform.forward;
+        float biggestClearDistance = 0;
+        RaycastHit hit;
+        for (int i = 0; i < _collisionDirections.Length; i++) { //Foreach points
+            Vector3 dir = transform.TransformDirection(_collisionDirections[i]); //fetch dir in worldspace
+            if (Physics.Raycast(transform.position, dir, out hit, _fovRadius)) {
+                if (hit.distance > biggestClearDistance) {
+                    Debug.DrawRay(transform.position, dir * _fovRadius, Color.red);
+                    bestDirection = dir;
+                    biggestClearDistance = hit.distance;
+                }
+            } else { 
+                Debug.DrawRay(transform.position, dir * _fovRadius, Color.green);
+                return dir; //Clear so return clear dir
+            }
+        }
+        Debug.DrawRay(transform.position, bestDirection * _fovRadius, Color.green);
+        return bestDirection; //No clear so retun biggest distance before impact
     }
 }
 
